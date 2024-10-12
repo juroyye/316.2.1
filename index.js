4c26838ac324a82bcd668a426eb7aa5ab50504e6
Justin M <manningjustin08@yahoo.com>
2024-10-12 11:37:08 -0400

    added countdown functionality

---
+++ index.js
@@ -0,0 +1,45 @@
+// Create your game here!
+const main_div = document.querySelector("div");
+const main_text = document.querySelector("h1");
+
+const attempts = document.createElement("h2");
+attempts.textContent = `You have 10 attempts remaining!`;
+main_text.append(attempts)
+const btn = document.querySelector("button")
+
+const winningNum = "35";
+let countdown = 10;
+
+btn.addEventListener("click", () => {
+    let userInput = window.prompt("Guess a number 1-100!")
+
+    if(userInput !== winningNum){
+        window.alert("Nope! Try again!")
+        decrement()
+        changeText()
+    } else{
+        window.alert("You're a genius! You won!")
+        attempts.textContent = `Did you read my mind??!`
+    }
+} )
+
+
+
+
+function decrement() {
+ countdown = countdown - 1
+}
+function changeText(){
+    attempts.textContent = `You have ${countdown} attempts remaining!`
+}
+
+
+
+
+
+
+
+
+
+
+
